6e9a92a1206c3d8094c49c5257b973b1f531c425
gkovan <gkovan@hotmail.com>
2021-06-24 13:13:18 -0400

    simplify logging (#10)

---
+++ src/main/java/com/ibm/mqclient/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package com.ibm.mqclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+	
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+          = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(100000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("###  CONTROLLER  ### REQUEST DATA : ");
+        return filter;
+    }
+}
